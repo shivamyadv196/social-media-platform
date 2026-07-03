0c0e2257e91c04bfc8b328e98bd19f070c95bc3b
shivamyadv196 <shivamYadv196@gmail.com>
2026-07-03 12:01:17 +0530

    comment section update

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -11,8 +11,8 @@ const userSchema = new mongoose.Schema({
    type:String,
    enum:['male','female'],
    default:null
-       }
-       
+       },
+
     followers:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}],
     following:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}],
     posts:[{type:mongoose.Schema.Types.ObjectId, ref:'Post'}],
