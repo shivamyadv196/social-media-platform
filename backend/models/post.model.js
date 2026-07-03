331604b4a3769e3ee0073946b036241323cc3867
shivamyadv196 <shivamYadv196@gmail.com>
2026-07-03 14:28:39 +0530

    sidebar update

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
     caption:{type:String, default:''},
     image:{type:String, required:true},
+
+
     author:{type:mongoose.Schema.Types.ObjectId, ref:'User', required:true},
     likes:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}],
     comments:[{type:mongoose.Schema.Types.ObjectId, ref:'Comment'}],
